2954e7829fd4159a3ab3ffb3700af7a999318acc
Travis CI <travis@travis-ci.org>
2018-09-18 13:23:13 +0200

    Fixed so that repo in push.sh is auto

--- push.sh
+++ push.sh
@@ -12,17 +12,21 @@ commit_pdfs() {
   git checkout --orphan "travis-$TRAVIS_BUILD_NUMBER"
   git rm --cached $(git ls-files)
   echo `ls tests`
-  git add "$TRAVIS_BUILD_DIR/tests/pdfs/"
+  git add -f "$TRAVIS_BUILD_DIR/tests/pdfs/"
   echo `git status`
   git commit --message "Travis build: $TRAVIS_BUILD_NUMBER"
 }
 
 upload_files() {
-  git remote add origin-login https://${GH_TOKEN}@github.com/Strauman/exercisebank.git
+  git remote add origin-login "https://${GH_TOKEN}@github.com/$TRAVIS_REPO_SLUG.git"
   git push --set-upstream -f origin-login "travis-$TRAVIS_BUILD_NUMBER"
   echo "PUSHED PDFS TO BRANCH travis-$TRAVIS_BUILD_NUMBER"
 }
-
-setup_git
-commit_pdfs
-upload_files
+# Only execute if branch doesn't start with travis-
+if [[ $TRAVIS_BRANCH == travis-* ]]; then
+  echo "On a travis branch. Not pushing."
+else
+  setup_git
+  commit_pdfs
+  upload_files
+fi
